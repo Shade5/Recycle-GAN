676b41deb55d99b4b2e50f32af98fa7dfa3cc5e8
Shade5 <george@wayve.ai>
2019-06-13 10:10:18 +0100

    Fixed indent error and added gitignore

--- data/base_data_loader.py
+++ data/base_data_loader.py
@@ -1,14 +1,10 @@
-
 class BaseDataLoader():
-    def __init__(self):
-        pass
-    
-    def initialize(self, opt):
-        self.opt = opt
-        pass
+	def __init__(self):
+		pass
 
-    def load_data():
-        return None
+	def initialize(self, opt):
+		self.opt = opt
+		pass
 
-        
-        
+	def load_data():
+		return None
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
-import time
 import os
-from options.test_options import TestOptions
+
 from data.data_loader import CreateDataLoader
 from models.models import create_model
-from util.visualizer import Visualizer
+from options.test_options import TestOptions
 from util import html
+from util.visualizer import Visualizer
 
 opt = TestOptions().parse()
-opt.nThreads = 1   # test code only supports nThreads = 1
+opt.nThreads = 1  # test code only supports nThreads = 1
 opt.batchSize = 1  # test code only supports batchSize = 1
 opt.serial_batches = True  # no shuffle
 opt.no_flip = True  # no flip
@@ -21,13 +21,13 @@ web_dir = os.path.join(opt.results_dir, opt.name, '%s_%s' % (opt.phase, opt.whic
 webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.which_epoch))
 # test
 for i, data in enumerate(dataset):
-    if i >= opt.how_many:
-        break
-    model.set_input(data)
-    model.test()
-    visuals = model.get_current_visuals()
-    img_path = model.get_image_paths()
-    print('%04d: process image... %s' % (i, img_path))
-    visualizer.save_images(webpage, visuals, img_path)
+	if i >= opt.how_many:
+		break
+	model.set_input(data)
+	model.test()
+	visuals = model.get_current_visuals()
+	img_path = model.get_image_paths()
+	print('%04d: process image... %s' % (i, img_path))
+	visualizer.save_images(webpage, visuals, img_path)
 
 webpage.save()
